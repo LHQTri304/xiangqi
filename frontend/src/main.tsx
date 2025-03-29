ba466f957ea046ed13707a3076ef9dcee0d2379e
012e <huyphmnhat@gmail.com>
2025-03-29 15:13:01 +0700

    change order of providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,40 +1,43 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import {Auth0Provider} from "@auth0/auth0-react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App.tsx";
-import {BrowserRouter, Route, Routes} from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Token from "./Token.tsx";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import OnlineGame from "./OnlineGame.tsx";
-import {StompSessionProvider} from "react-stomp-hooks";
+import { StompSessionProvider } from "react-stomp-hooks";
 import NewGame from "./NewGame.tsx";
 
-const BACKEND_URL = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://xiangqi-backend-e4f524a5a2ad.herokuapp.com";
- 
+const BACKEND_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : "https://xiangqi-backend-e4f524a5a2ad.herokuapp.com";
+
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Auth0Provider
+        domain="dev-l5aemj1026u4dqia.us.auth0.com"
+        clientId="3WDtuCDz2foYFJAHjKc2FxTTJmplDfL6"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: "xiangqi-backend",
+        }}
+      >
         <StompSessionProvider url={`${BACKEND_URL}/ws`}>
-            <Auth0Provider
-                domain="dev-l5aemj1026u4dqia.us.auth0.com"
-                clientId="3WDtuCDz2foYFJAHjKc2FxTTJmplDfL6"
-                authorizationParams={{
-                    redirect_uri: window.location.origin,
-                    audience: "xiangqi-backend",
-                }}
-            >
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<App/>}/>
-                        <Route path="/token" element={<Token/>}/>
-                        <Route path="/game/new" element={<NewGame/>}/>
-                        <Route path="/game/:id" element={<OnlineGame/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </Auth0Provider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/token" element={<Token />} />
+              <Route path="/game/new" element={<NewGame />} />
+              <Route path="/game/:id" element={<OnlineGame />} />
+            </Routes>
+          </BrowserRouter>
         </StompSessionProvider>
+      </Auth0Provider>
     </QueryClientProvider>
   </StrictMode>,
 );
