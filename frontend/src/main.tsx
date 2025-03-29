52b38933f6d356c79f51f5eb931364492ea2e087
012e <huyphmnhat@gmail.com>
2025-03-29 13:46:58 +0700

    trigger build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import OnlineGame from "./OnlineGame.tsx";
 import {StompSessionProvider} from "react-stomp-hooks";
 import NewGame from "./NewGame.tsx";
-
+ 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
